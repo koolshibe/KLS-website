f8ae7e968ca6ea3d289810b1a15b5d497e0cb53f
koolshibe <porgsgomeep@gmail.com>
2025-04-16 15:54:25 -0700

    Basic content, *untested* slug

--- khanlabstudios/next.config.mjs
+++ khanlabstudios/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+        // Basic redirect
+        {
+            source: '/about',
+            destination: '/',
+            permanent: true,
+        }
+        ]
+    }
+};
 
 export default nextConfig;
---
+++ khanlabstudios/src/app/layout.js
@@ -0,0 +1,11 @@
+export default function DashboardLayout({
+    children,
+  }) {
+    return (
+      <html lang="en">
+        <body>
+          <main>{children}</main>
+        </body>
+      </html>
+    )
+  }
