bd5ec778cc37630544ab55204dfbe7c79c9661f2
Bobb.happybook <bobb@happybook>
2025-05-08 14:38:37 -0700

    Add global css, title animation

--- khanlabstudios/src/pages/index.js
+++ khanlabstudios/src/pages/index.js
@@ -1,7 +1,15 @@
+import styles from '../globals.module.css';
+
 export default function Page() {
     return (
-        <div>
-            KYS
+        <div className="container text-center mt-5">
+            <h1 
+                className={styles.title}>
+                Welcome to Khan Lab Studios
+            </h1>
+            <p className={styles.description}>
+                We are a team of passionate developers and designers.
+            </p>
         </div>
     );
 }
