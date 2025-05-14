f81f30082128dc6e3421158c2e8f5bcf5c7c0351
vk2009 <67607214+vk2009@users.noreply.github.com>
2025-05-13 19:11:41 -0700

    i fixed the overlapping thing, storycards should break line with a gap. Requires more testing on phones and smaller viewport devices

--- khanlabstudios/src/pages/components/story.js
+++ khanlabstudios/src/pages/components/story.js
@@ -11,11 +11,13 @@ export default function Story({title, student, summary, date, storyID}) {
     let newStudent = student.toUpperCase();
     return (
       <div id={styles.storyCard}> 
+        <div className={styles.smallContainer}>
         <h4 className={styles.cardStudent}><strong>{newStudent}</strong></h4><span className={styles.breakLine}></span>
         <p className={styles.cardDate}>{date}</p><span className={styles.breakLine}></span>
         <h1 className={styles.cardTitle}><strong>{title}</strong></h1><span className={styles.breakLine}></span>
         <p className={styles.cardSummary}>{summary}</p><span className={styles.breakLine}></span>
         <button className={styles.cardButton} onClick={handleClick}><strong>EXPLORE</strong></button>
+        </div>
       </div>
     );
 }
