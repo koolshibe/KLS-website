8c721fee00060095a3a913b29bcc5c746be72185
koolshibe <porgsgomeep@gmail.com>
2025-05-15 13:40:06 -0700

    Added dept. pages

---
+++ khanlabstudios/src/app/departments/page.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <div>
+        <Link href='/website'>Website</Link>
+        <Link href='/marcomm'>Marcomm</Link>
+        <Link href='/art'>Art</Link>
+    </div>
+  )
+}
