dcafd90613aafbf07b2c551ac961e3dac8b692ad
Bobb.happybook <bobb@happybook>
2025-05-15 16:04:24 -0700

    Update member list

--- khanlabstudios/src/app/components/member.js
+++ khanlabstudios/src/app/components/member.js
@@ -1,15 +1,16 @@
 import styles from '../../globals.module.css';
 import Link from 'next/link';
 
-export function MemberCard({ student, bio, studentID }) {
+export function MemberCard({ student, bio, studentID , department }) {
   const newStudent = student.toUpperCase();
 
   return (
     <div className={styles.cardLink}>
       <div className={styles.memberCard}>
-        <img src={`/students/${newStudent}.jpeg`} alt={newStudent} className={styles.cardImage} />
+        <img src={`/students/${newStudent}.jpg`} alt={newStudent} className={styles.cardImage} />
         <h4 className={styles.cardStudent1}><strong>{newStudent}</strong></h4>
         <span className={styles.breakLine}></span>
+        <p className={styles.cardStudentDepartment}><strong>{department}</strong></p>
         <p className={styles.cardBio}>{bio}</p>
       </div>
     </div>
