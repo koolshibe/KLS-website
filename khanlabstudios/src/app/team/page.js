e4a4c6729f660a18e262a373eea37d763d82615c
vk2009 <67607214+vk2009@users.noreply.github.com>
2025-05-16 20:40:30 -0700

    styled sidebar (up to change) and styled story cards better. fixed stories page

--- khanlabstudios/src/app/team/page.js
+++ khanlabstudios/src/app/team/page.js
@@ -1,9 +1,9 @@
 import styles from '../../globals.module.css';
 import {MemberCard} from '@/app/components/member.js';
-
 export default function Page() {
     return (
         <div className="container text-center mt-5">
+            
             <MemberCard student={"Abhinav"} bio={"Bob."} studentID={2} department={"Website"}/>
             <MemberCard student={"Vidit"} bio={"Vidit is a curious and creative learner who loves exploring new ideas. Whether he's building something with code, solving tricky puzzles, or diving into a new book, Vidit always brings enthusiasm and a positive attitude. He enjoys working with friends, asking great questions, and finding fun ways to learn and grow every day. "} studentID={3} department={"Website"}/>
             <MemberCard student={"Ankur"} bio={"Ankur is a passionate and curious learner who loves to explore new ideas and concepts. He enjoys working on projects that challenge him to think critically and creatively. In his free time, Ankur likes to read, play video games, and spend time with friends."} studentID={4} department={"Website"}/>
