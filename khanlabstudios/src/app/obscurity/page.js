d6cf3fc7f8d9dccc35fcd2220ade80ccbb8a4441
Bobb.happybook <bobb@happybook>
2025-05-19 13:49:54 -0700

    Updated css for upload page

--- khanlabstudios/src/app/obscurity/page.js
+++ khanlabstudios/src/app/obscurity/page.js
@@ -1,18 +1,18 @@
 'use client'
 
 import { upload } from './upload.js';
+import styles from '../../globals.module.css';
 
 export default function Page() {
   return (
-    <form>
-      <label htmlFor="title">Title: </label>
-      <input id="title" name="title" type="text" required />
-      <label htmlFor="author">Author: </label>
-      <input id="author" name="author" type="text" required />
-      <label htmlFor="file">Markdown text: </label>
-      <label htmlFor="files">Images: </label>
-      <input type="file" id="files" name="files" multiple/>
-      <button formAction={upload}>upload</button>
+    <form className={styles.form}>
+      <label htmlFor="title" className={styles.label}>Title: </label>
+      <input id="title" name="title" type="text" required className={styles.input} />
+      <label htmlFor="author" className={styles.label}>Author: </label>
+      <input id="author" name="author" type="text" required className={styles.input} />
+      <label htmlFor="files" className={styles.label}>Images: </label>
+      <input type="file" id="files" name="files" multiple className={styles.input} />
+      <button formAction={upload} className={styles.button}>upload</button>
     </form>
   )
 }
