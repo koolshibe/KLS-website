46a25461d44f1ed34a8070812d6a8a4315c89e73
koolshibe <porgsgomeep@gmail.com>
2025-06-05 14:40:43 -0700

    Added authors to author page

--- khanlabstudios/src/app/stories/[slug]/page.js
+++ khanlabstudios/src/app/stories/[slug]/page.js
@@ -7,17 +7,19 @@ export async function storyfetch(context) {
 
     const { data, error } = await supabase
         .from("stories")
-        .select()
+        .select('title, published, state, id, authors!inner(author), summary')
         .eq('id', context)
+
+    console.log(data)
 
     
     const url = `https://hb2d7h9foo6iagrh.public.blob.vercel-storage.com/${data[0]['id']}`;
     const story = ((await fetch(`${url}/story.txt`).then(response => response.text())))
 
     return (
-        <div key="exist">
+        <div>
             <h1 className={styles.storyTitle}>{data[0]["title"]}</h1>
-            <h2 className={styles.storyAuthor}>{data[0]["author"]}</h2>
+            <h2 className={styles.storyAuthor}>{data[0]["authors"].map((x)=>`${x['author']}`)}</h2>
             <h4 className={styles.storyDate}>{data[0]["published"]}</h4>
             <div className={styles.storyText}>
                 {story.toString().split(/\r\n|\n|\r/).map( (element, index) =>
@@ -32,7 +34,7 @@ export default async function Page({params}) {
   const {slug} = await params;
 
   const data = storyfetch(slug);
-
+  
   return (
     <div className={styles.storyBody}>
       {data}
