0706f4f35d223d8ca52cbafc95c6a2c8cb424986
koolshibe <porgsgomeep@gmail.com>
2025-06-04 18:39:03 -0700

    Fixed transition links

--- khanlabstudios/src/app/template.js
+++ khanlabstudios/src/app/template.js
@@ -3,24 +3,33 @@
 import { motion } from "motion/react";
 import { animate } from 'motion';
 import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation'
 
 export default function Template({ children }) {
 
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
   useEffect(() => {
-        const onPageLoad = () => {
+    const onPageLoad = () => {
             console.log('page loaded');
             animate('#exit', { rotateY: 90 }, { duration: 0.75 });
-        };
+    };
 
-        // Check if the page has already loaded
-        if (document.readyState === 'complete') {
-            onPageLoad();
-        } else {
-            window.addEventListener('load', onPageLoad, false);
-            // Remove the event listener when component unmounts
-            return () => window.removeEventListener('load', onPageLoad);
-        }
-  })
+    // Check if the page has already loaded
+    // if (document.readyState == 'complete' || document.readyState == 'interactive') {
+    onPageLoad();
+    console.log(pathname)
+  }, [pathname, searchParams])
+
+  //useEffect(() => {
+        
+        // } else {
+        //     window.addEventListener('load', onPageLoad, false);
+        //     // Remove the event listener when component unmounts
+        //     return () => window.removeEventListener('load', onPageLoad);
+        // }
+  //})
 
   return ( 
       <motion.div
